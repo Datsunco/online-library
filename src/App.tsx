f7fb91f79a3577d5bd56fc2681b6b0c9ffc43481
DatsunCo <danya200375d@gmail.com>
2024-02-14 12:39:45 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Input } from './components/ui/input'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Input/>
+    </>
+  )
+}
+
+export default App
