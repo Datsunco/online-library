777a404af8f57507aa2c4d9724442b594de35f83
DatsunCo <danya200375d@gmail.com>
2024-02-20 10:18:22 +0300

    dev: design separayte padge

--- src/pages/separatePage.tsx
+++ src/pages/separatePage.tsx
@@ -1,22 +1,46 @@
 import { useAppDispath, useAppSelector } from '@/hooks/redux';
 import { fetchCurrentBook } from '@/store/reducers/ActionCreators';
+import { Label } from "@/components/ui/label"
+import { Badge } from '@/components/ui/badge';
+import { Textarea } from "@/components/ui/textarea"
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Card, CardContent } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const SeparatePage = () => {
-    const {title, image} = useAppSelector(state => state.currentBooksSlicer)
     const { id } = useParams()
     const dispatch = useAppDispath()
+    const { title, image, categories, description, authors, isLoading} = useAppSelector(state => state.currentBooksSlicer)
+
 
     useEffect(() => {
         dispatch(fetchCurrentBook(id!))
     }, [])
 
     return (
-        <div>
-            <img src={image}/>
-            {title}
-            {id}
+        <div className="flex">
+            <div className="">
+                <img src={image} />
+            </div>
+            { !isLoading ? 
+            <div className="w-[600px] flex flex-col gap-3">
+                <Badge>{categories} </Badge>
+                {/* <Label htmlFor="framework"> {categories} </Label> */}
+                <Label htmlFor="title"> {title} </Label>
+                <Badge variant="outline">{authors} </Badge>
+                {/* <Textarea className='h-fulll' disabled={true} placeholder={description}/> */}
+                <Card>
+                    <CardContent>
+                        {description}
+                    </CardContent>
+                </Card>
+            </div>
+            :
+            <Skeleton>
+
+            </Skeleton>
+            }
         </div>
     );
 };
